e07693241d2bdd8a690ba566390bd3c546623b1e
Kevin Wood <krwood90@gmail.com>
2017-11-09 17:28:40 -0600

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
-class User < ApplicationRecord
+require 'bcrypt'
+
+class User < ActiveRecord
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 end
